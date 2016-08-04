a542008d87f89c589c2f740927179450bb9ef3f7
dwalker711 <dwalker711@gmail.com>
2016-08-03 21:43:21 -0400

    Tired and watching twitch - 8/3/16 9:43PM

---
+++ Chapter6/tiy_6_7.py
@@ -0,0 +1,13 @@
+# 6-7 People
+
+peeps = {
+	'sanic': {
+		'hedgehog': 'gotta go fast!',
+		'naked': 'blue spikey things'
+		},
+	'knuckles': {
+		'echinda': 'PAWNCH',
+		'naked': "red jumpsuit thingy"
+		}
+	}
+
