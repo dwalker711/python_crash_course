06032a25aaac9414ef58e15e950db042c79afcf3
dwalker711 <dwalker711@gmail.com>
2016-09-06 23:46:13 -0400

    9/6/2016

--- Chapter9/tiy_9_4.py
+++ Chapter9/tiy_9_4.py
@@ -17,3 +17,30 @@ class Restaurant():
 		'''Prints messge indicating the restaurant is open.'''
 		print("\n" + self.restaurant_name + " is now open.")
 
+	def guest_count(self):
+		'''Print count of guests that have been served.'''
+		print(str(self.number_served) + " guests have currently been served.")
+
+	def set_number_served(self, num_guests):
+		'''Sets the number of guests served.'''
+		self.number_served = num_guests
+
+	def increment_number_served(self, num_gests2):
+		'''Add the amount to the count of guests.'''
+		self.number_served += num_gests2
+
+r1 = Restaurant('carabas', 'italian')
+r1.describe_restaurant()
+
+# Manually changing the number of customers served
+r1.number_served = 45
+r1.guest_count()
+
+# Using a method to set the number of guests that have been served
+r1.set_number_served(50)
+r1.guest_count()
+
+# Using a method to INCREMENT the number of guests that have been served
+r1.increment_number_served(-12)
+r1.guest_count()
+
